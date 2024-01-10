1b7cd51fa3a397a974bd1bf1e6be1b66def448fe
ValeriiaProkhina <140752336+ValeriiaProkhina@users.noreply.github.com>
2024-01-10 19:39:52 +0200

    update

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+// refresh() Бібліотека містить метод [refresh()](https://github.com/andreknieriem/simplelightbox#public-methods), який обов'язково потрібно викликати щоразу після додавання нових елементів до галереї.
