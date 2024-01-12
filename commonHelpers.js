eefb463bb649a79c94ac9e4160d9d193c0379ef8
ValeriiaProkhina <140752336+ValeriiaProkhina@users.noreply.github.com>
2024-01-12 20:24:22 +0000

    Deploying to gh-pages from @ ValeriiaProkhina/goit-js-hw-11@719c363bd84ad88b8ce1a35eb45b95553fc447d7 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,30 +1,31 @@
-import{S as c,i as l}from"./assets/vendor-9310f15c.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const n=document.querySelector(".search-form"),d=document.querySelector(".gallery");document.querySelector(".loader");const u="https://pixabay.com/api",p="41747369-46a857856bf510ac3748d6666";n.addEventListener("submit",s=>{s.preventDefault();const t=s.currentTarget.elements.query.value,r=new URLSearchParams({key:p,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0});h(r),n.reset()});const f=s=>fetch(`${u}?${s}`).then(t=>{if(t.ok)return t.json();throw new Error("Sorry, there are no images matching your search query. Please try again!")}),m=s=>s.hits.reduce((t,r)=>t+`<li class="img-item">
-  <a class="img-link" href="${r.largeImageURL}">
+import{S as l,i as d}from"./assets/vendor-9310f15c.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))i(r);new MutationObserver(r=>{for(const o of r)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function t(r){const o={};return r.integrity&&(o.integrity=r.integrity),r.referrerpolicy&&(o.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?o.credentials="include":r.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(r){if(r.ep)return;r.ep=!0;const o=t(r);fetch(r.href,o)}})();const n=document.querySelector(".search-form"),u=document.querySelector(".gallery"),c=document.querySelector(".span-loader"),p="https://pixabay.com/api/",m="41747369-46a857856bf510ac3748d6666";n.addEventListener("submit",s=>{s.preventDefault(),c.classList.add("loader");const e=s.currentTarget.elements.query.value,t=new URLSearchParams({key:m,q:e,image_type:"photo",orientation:"horizontal",safesearch:!0});g(t),n.reset()});const f=s=>fetch(`${p}?${s}`).then(e=>{if(e.ok)return e.json();throw new Error("Sorry, there are no images matching your search query. Please try again!")}),h=s=>s.hits.reduce((e,t)=>e+`<li class="img-item">
+      <div class="img">
+  <a class="img-link" href="${t.largeImageURL}">
     <img
       class="images"
-      src="${r.webformatURL}"
-      data-source="${r.largeImageURL}"
-      alt="${r.tags}"
+      src="${t.webformatURL}"
+      data-source="${t.largeImageURL}"
+      alt="${t.tags}"
       width="360"
       height="200"
-  /></a>
+  /></a></div>
 <div class="description">
-  <div>
+  <div class="category">
     <p><b>Likes</b></p>
-    <p>${r.likes}</p>
+    <p>${t.likes}</p>
   </div>
   <div>
     <p><b>Views</b></p>
-    <p>${r.views}</p>
+    <p>${t.views}</p>
   </div>
   <div>
     <p><b>Comments</b></p>
-    <p>${r.comments}</p>
+    <p>${t.comments}</p>
   </div>
   <div>
     <p><b>Downloads</b></p>
-    <p>${r.downloads}</p>
+    <p>${t.downloads}</p>
   </div>
 </div>
-</li>`,"");function h(s){f(s).then(t=>{const r=new c(".gallery a",{captionsData:"alt",captionDelay:250,close:!0});d.insertAdjacentHTML("afterbegin",m(t)),r.refresh()}).catch(t=>l.error({message:t.message,position:"topRight"}))}
+</li>`,"");function g(s){f(s).then(e=>{if(e.hits.length===0)throw new Error("Sorry, there are no images matching your search query. Please try again!");const t=new l(".gallery a",{captionsData:"alt",captionDelay:250,close:!0});u.insertAdjacentHTML("afterbegin",h(e)),t.refresh()}).catch(e=>d.error({message:e.message,position:"topRight"})).finally(()=>{c.classList.remove("loader")})}
 //# sourceMappingURL=commonHelpers.js.map
